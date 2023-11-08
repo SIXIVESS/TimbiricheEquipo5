8453bf1ddabd7952d38f757588b1a39c79997ba4
SIXIVESS <mettatonbb@gmail.com>
2023-11-08 00:50:18 -0700

    frmUnirseJuego lista

---
+++ Timbiriche/src/main/java/modelo/IUnirseJuego.java
@@ -0,0 +1,15 @@
+package modelo;
+
+/**
+ *
+ * @author chaly
+ */
+public interface IUnirseJuego {
+
+    public void ingresarCodigo();
+
+    public void siguiente();
+
+    public void regresarMenu();
+
+}
