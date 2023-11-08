a3fca079d8d981b0ef1167f46d21b53f3f21e82c
SIXIVESS <mettatonbb@gmail.com>
2023-11-08 01:06:18 -0700

    frms

---
+++ Timbiriche/src/main/java/modelo/ICrearJuego.java
@@ -0,0 +1,16 @@
+package modelo;
+
+/**
+ *
+ * @author chaly
+ */
+public interface ICrearJuego {
+
+    public void cantidadJugador();
+
+    public void crearJuego();
+
+    public void siguiente();
+
+    public void regresarMenu();
+}
