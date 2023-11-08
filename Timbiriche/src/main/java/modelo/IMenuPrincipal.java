fd95ffd65975bcb5539712be948028dabf57e93c
SIXIVESS <mettatonbb@gmail.com>
2023-11-07 23:10:48 -0700

    Cambio de clases

---
+++ Timbiriche/src/main/java/modelo/IMenuPrincipal.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public interface IMenuPrincipal {
+    public void btnCrear();
+    
+    public void btnUnire();
+    
+    public void abrirMenu();
+    
+    public void salir();
+}
